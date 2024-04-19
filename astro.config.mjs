41ccf47afab98124d9615488dbf7bf01490892f9
DejFcoder <trebatickydavid670@gmail.com>
2024-04-19 17:22:23 +0200

    netlify functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
